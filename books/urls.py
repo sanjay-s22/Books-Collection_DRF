285dda9f03c09eb7e297153148d721e0015cf030
sanjay <71496141+wolves-coder@users.noreply.github.com>
2025-09-01 01:59:51 +0530

    cleaned up code

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list, name='book-list'),
-    path('books/<int:book_id>/', views.book_detail, name='book-detail'),
+    path('books/', views.book_list, name='book-list'), # List/create books
+    path('books/<int:book_id>/', views.book_detail, name='book-detail'), # Get/update/delete specific book
 ]
